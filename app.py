fe973b7de3e40fb5685d158a3115098ff63f6f83
pagutaro-o0 <yuiopi.yuiopi.o00@gmail.com>
2026-02-13 16:34:08 +0900

    Fix: remove app=create_app for gunicorn

--- app.py
+++ app.py
@@ -379,6 +379,6 @@ def create_app():
 
 if __name__ == "__main__":
     import os
-    app = create_app()
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
