69d8bffcdc172d18a05347bcdbcc44c729c7466b
pagutaro-o0 <yuiopi.yuiopi.o00@gmail.com>
2026-02-13 13:55:33 +0900

    Prepare for Render deploy

--- app.py
+++ app.py
@@ -566,7 +566,10 @@ def create_app():
 
     return app
 
+app = create_app()
+
+import os
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", "5001"))
+    app.run(debug=True, host="0.0.0.0", port=port)
